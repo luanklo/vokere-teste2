154e2c6a91ef5f50d2c3595a0c709e4c9fc837e1
luanklo <luanj.kloh@hotmail.com>
2024-06-17 22:25:22 -0300

    mudança nos visuais

--- resources/views/Admin/ClientForm.blade.php
+++ resources/views/Admin/ClientForm.blade.php
@@ -1,40 +1,60 @@
-<h1>Novo Usuraio</h1>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Novo Cliente') }}
+        </h2>
+    </x-slot>
 
-@if ($errors->any())
-    <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-@endif
+    @if ($errors->any())
+        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+            <strong class="font-bold">Oops!</strong>
+            <span class="block sm:inline">Algo deu errado.</span>
+            <ul class="mt-2">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
-<form action="{{ route('ClientForm.store') }}" method="POST">
-    @csrf
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-6">
+                <form action="{{ route('ClientForm.store') }}" method="POST" class="space-y-6">
+                    @csrf
 
-    <div>
-        <x-label for="name" value="{{ __('Name') }}" />
-        <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                    <div>
+                        <x-label for="name" value="{{ __('Name') }}" />
+                        <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                    </div>
+            
+                    <div class="mt-4">
+                        <x-label for="cpf" value="{{ __('CPF') }}" />
+                        <x-input id="cpf" class="block mt-1 w-full" type="text" name="cpf" :value="old('cpf')" required autocomplete="cpf" />
+                    </div>
+            
+                    <div class="mt-4">
+                        <x-label for="date_of_birth" value="{{ __('Data de Nascimento') }}" />
+                        <x-input id="date_of_birth" class="block mt-1 w-full" type="date" name="date_of_birth" :value="old('date_of_birth')" required autocomplete="bdate" />
+                    </div>
+            
+                    <div class="mt-4">
+                        <x-label for="email" value="{{ __('Email') }}" />
+                        <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                    </div>
+            
+                    <div class="mt-4">
+                        <x-label for="password" value="{{ __('Password') }}" />
+                        <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                    </div>
+
+                    <div class="flex items-center justify-end">
+                        <x-button class="ml-4">
+                            {{ __('Salvar') }}
+                        </x-button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
-
-    <div class="mt-4">
-        <x-label for="cpf" value="{{ __('CPF') }}" />
-        <x-input id="cpf" class="block mt-1 w-full" type="text" name="cpf" :value="old('cpf')" required autocomplete="cpf" />
-    </div>
-
-    <div class="mt-4">
-        <x-label for="date_of_birth" value="{{ __('Data de Nascimento') }}" />
-        <x-input id="date_of_birth" class="block mt-1 w-full" type="date" name="date_of_birth" :value="old('date_of_birth')" required autocomplete="bdate" />
-    </div>
-
-    <div class="mt-4">
-        <x-label for="email" value="{{ __('Email') }}" />
-        <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-    </div>
-
-    <div class="mt-4">
-        <x-label for="password" value="{{ __('Password') }}" />
-        <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-    </div>
-
-    <button type="submit">salvar</button>
-</form>
+</x-app-layout>
--- resources/views/Admin/ClientList.blade.php
+++ resources/views/Admin/ClientList.blade.php
@@ -1,47 +1,64 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Clientes</h1>
-    
-    @if (session()->has('success'))
-    {{ session('success') }}
-    @endif
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Edição de Cliente') }}
+        </h2>
+    </x-slot>
 
-    <a href="{{route('ClientForm.create')}}">Novo CLiente</a>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-6">
+                @if (session()->has('success'))
+                    <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
+                        <strong class="font-bold">Sucesso!</strong>
+                        <span class="block sm:inline">{{ session('success') }}</span>
+                    </div>
+                @endif
 
-    <table>
-        <thead>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Data de Nascimento</th>
-            <th>Data de Cadastro</th>
-            <th>Ações</th>
-        </thead>
-        <tbody>
-            @forelse ($clients as $client)
-                <tr>
-                    <td>{{ $client->id }}</td>
-                    <td>{{ $client->name }}</td>
-                    <td>{{ $client->date_of_birth }}</td>
-                    <td>{{ $client->created_at }}</td>
-                    <td><a href="{{ route('ClientEdit.edit', $client->id)}}">Edição</a></td>
-                </tr>
-            
-                @empty
-                <tr>
-                    <td colspan="100">Nenhum cliente no bando de dados</td>
-                </tr>
-            @endforelse
-        </tbody>
-    </table>
+                <div class="flex justify mb-4">
+                    <a href="{{ route('ClientForm.create') }}" class="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest">
+                        Novo Cliente
+                    </a>
+                </div>
 
+                <table class="w-full dark:bg-gray-800">
+                    <thead>
+                        <tr>
+                            <th class="px-6 py-4 text-white border-b text-left dark:border-gray-700">ID</th>
+                            <th class="px-6 py-4 text-white border-b text-left dark:border-gray-700">Nome</th>
+                            <th class="px-6 py-4 text-white border-b text-left dark:border-gray-700">Data de Nascimento</th>
+                            <th class="px-6 py-4 text-white border-b text-left dark:border-gray-700">Data de Cadastro</th>
+                            <th class="px-6 py-4 text-white border-b text-left dark:border-gray-700">Ações</th>
+                        </tr>
+                    </thead>    
+                    <tbody>
+                        @forelse ($clients as $client)
+                            <tr>
+                                <td class="px-6 py-4 text-white border-b text-center dark:border-gray-700">{{ $client->id }}</td>
+                                <td class="px-6 py-4 text-white border-b text-center dark:border-gray-700">{{ $client->name }}</td>
+                                <td class="px-6 py-4 text-white border-b text-center dark:border-gray-700">{{ $client->date_of_birth }}</td>
+                                <td class="px-6 py-4 text-white border-b text-center dark:border-gray-700">{{ $client->created_at }}</td>
+                                <td class="px-6 py-4 text-white border-b text-center dark:border-gray-700">
+                                    <a href="{{ route('ClientEdit.edit', $client->id) }}" class="text-white bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest ">Edição</a>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="5" class="px-6 py-4 whitespace-nowrap text-center text-gray-500 dark:text-gray-400">Nenhum cliente no banco de dados</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
 
-    {{ $clients->links() }}
-</body>
-</html>
+                <div class="mt-4">
+                    {{ $clients->links() }}
+                </div>
+
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-6 w-1/3">
+                    <h3 class="text-lg font-semibold text-gray-800 dark:text-gray-200 mb-4">Nova Seção</h3>
+                    <p class="text-gray-600 dark:text-gray-400">Esta é uma nova seção adicionada ao lado do dashboard. Você pode colocar qualquer conteúdo aqui, como gráficos, informações adicionais, etc.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
