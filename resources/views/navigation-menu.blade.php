496c27772a98733cab8c5ab07cb14d603d4c94fb
luanklo <luanj.kloh@hotmail.com>
2024-06-17 22:30:16 -0300

    adicionado a lista de clientes no menu

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -16,6 +16,12 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link href="{{ route('ClientList.index') }}" :active="request()->routeIs('dashboard')">
+                        {{ __('Clientes') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
