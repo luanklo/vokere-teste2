15e9959b49a39770020c68622bb7b6d3163b4bf5
luanklo <luanj.kloh@hotmail.com>
2024-06-23 21:45:44 -0300

    Criação da entidade endereço

---
+++ database/migrations/2024_06_24_003623_address.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            # Rua, Número, Complemento, Bairro, Cidade, Estado, CEP
+            $table->id();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('street');
+            $table->string('number');
+            $table->string('complement');
+            $table->string('neighborhood');
+            $table->string('city');
+            $table->string('state');
+            $table->string('cep');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
